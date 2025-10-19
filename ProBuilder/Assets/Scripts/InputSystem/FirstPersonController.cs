d8f0f0b6f4307fc95533cd37a9b0ead9619a2cb5
David-best1 <tsaturov.david@yandex.ru>
2025-10-20 02:03:08 +0300

    create

---
+++ ProBuilder/Assets/Scripts/InputSystem/FirstPersonController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] public float jumpForce = 5f;
+    [SerializeField] public float lookSpeed = 2f;
+    [SerializeField] public Transform playerCamera;
+
+    [SerializeField] private Vector2 moveInput;
+    [SerializeField] private Vector2 lookInput;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float verticalLookRotation = 0f;
+
+    [SerializeField] private PlayerControls controls;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        // Инициализация Input System
+        controls = new PlayerControls();
+
+        rb = GetComponent<Rigidbody>();
+
+        controls.Gameplay.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Gameplay.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Look.canceled += ctx => lookInput = Vector2.zero;
+
+        controls.Gameplay.Jump.performed += ctx => OnJump();
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        Vector3 movement = transform.right * moveDirection.x + transform.forward * moveDirection.z;
+
+        rb.linearVelocity = new Vector3(movement.x * moveSpeed, rb.linearVelocity.y, movement.z * moveSpeed);
+    }
+
+    private void Update()
+    {
+        float lookX = lookInput.x * lookSpeed * Time.deltaTime;
+        float lookY = lookInput.y * lookSpeed * Time.deltaTime;
+
+        verticalLookRotation -= lookY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        playerCamera.localEulerAngles = new Vector3(verticalLookRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * lookX);
+    }
+
+    private void OnJump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
